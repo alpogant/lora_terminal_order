1b06d33dfd5bf7f8e34b0c4386a8ef8de08ea8c6
Alperen Demirkol <74583848+alpogant@users.noreply.github.com>
2022-03-22 09:09:02 +0300

    Update lora_rx_terminal_taker.py

--- lora_rx_terminal_taker.py
+++ lora_rx_terminal_taker.py
@@ -2,17 +2,36 @@ import subprocess
 import serial 
 from time import sleep
 
+def listToString(s): 
+
+	# initialize an empty string
+	str1 = "" 
+
+	# return string  
+	return (str1.join(s))
+
+
 ser = serial.Serial ("/dev/ttyUSB0", 9600)
+data = []
 
 while (1):
-    received_data = ser.read()              
-    sleep(0.03)
-    data_left = ser.inWaiting()             
-    received_data += ser.read(data_left)
-    print (received_data)                   
-    ser.write(received_data)
-    subprocess.call([received_data])
-
+	try:
+		received_data = ser.read()              
+		sleep(0.03)
+		data_left = ser.inWaiting()             
+		received_data += ser.read(data_left)
+		#print (received_data)                   
+		ser.write(received_data)
+		for i in received_data:
+			data.append(chr(i))
+		command = listToString(data)
+		subprocess.call([command])
+		data.clear()
+		command = ""
+	except:
+		print("Error")
+		data.clear()
+		command = ""
 
 
 
